e14b5739638b4138de3b73571f7146fa502342d9
Adilson Roberto Leonello <aleonello@gmail.com>
2019-01-27 12:03:29 -0200

    Create choco_upgrade.bat
    New script file to install or upgrade softwares on Windows

---
+++ choco_upgrade.bat
@@ -0,0 +1,21 @@
+choco upgrade ccleaner -y
+choco upgrade chocolatey -y
+choco upgrade dbeaver -y
+choco upgrade jdk8 -y
+choco upgrade jre8 -y
+choco upgrade filezilla -y
+choco upgrade firefox -y
+choco upgrade git.install -y
+choco upgrade gitextensions -y
+choco upgrade kdiff3 -y
+choco upgrade nodejs-lts -y
+choco upgrade postman -y
+choco upgrade putty -y
+choco upgrade sql-server-management-studio -y
+choco upgrade TortoiseGit -y
+@echo choco upgrade tortoisesvn -y
+choco upgrade VisualStudioCode -y
+choco upgrade vlc -y
+choco upgrade winmerge -y
+choco upgrade winscp -y
+@pause
