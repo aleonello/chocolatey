195a75a89823417698617e15b00196e501874d14
Adilson Roberto Leonello <Adilson.Leonello@tetrapak.com>
2019-01-28 08:44:02 -0200

    -Including some applications.

--- choco_upgrade.bat
+++ choco_upgrade.bat
@@ -5,10 +5,14 @@ choco upgrade jdk8 -y
 choco upgrade jre8 -y
 choco upgrade filezilla -y
 choco upgrade firefox -y
+choco upgrade freefilesync -y
 choco upgrade git.install -y
 choco upgrade gitextensions -y
+choco upgrade keepass -y
 choco upgrade kdiff3 -y
+choco upgrade lightshot -y
 choco upgrade nodejs-lts -y
+choco upgrade notepadplusplus -y
 choco upgrade postman -y
 choco upgrade putty -y
 choco upgrade sql-server-management-studio -y
@@ -18,4 +22,6 @@ choco upgrade VisualStudioCode -y
 choco upgrade vlc -y
 choco upgrade winmerge -y
 choco upgrade winscp -y
+@echo choco upgrade vcredist2013 -y
+@echo choco upgrade vcredist2017 -y
 @pause
