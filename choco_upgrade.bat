dba784ca87ae168a8862c18718992212c65e752b
Adilson Roberto Leonello <Adilson.Leonello@tetrapak.com>
2019-03-08 13:32:58 -0300

    -Script update

--- choco_upgrade.bat
+++ choco_upgrade.bat
@@ -1,29 +1,43 @@
+choco upgrade AstroGrep -y
+@echo choco upgrade autoit -y
 choco upgrade ccleaner -y
 choco upgrade chocolatey -y
+@echo choco upgrade ConEmu -y
 choco upgrade dbeaver -y
+choco upgrade duplicatefilefinder -y
 choco upgrade jdk8 -y
 choco upgrade jre8 -y
 choco upgrade filezilla -y
 choco upgrade firefox -y
 choco upgrade freefilesync -y
+@echo choco upgrade FreeDownloadManager -y
 choco upgrade git.install -y
 choco upgrade gitextensions -y
+choco upgrade ilspy -y
 choco upgrade keepass -y
 choco upgrade kdiff3 -y
 choco upgrade lightshot -y
 choco upgrade nodejs-lts -y
 choco upgrade notepadplusplus -y
+@echo choco upgrade nunit -y
+@echo choco upgrade PDFCreator -y
 choco upgrade pdfsam -y
+@echo choco upgrade picpick.portable -y
 choco upgrade postman -y
 choco upgrade putty -y
+choco upgrade soapui -y
 choco upgrade sql-server-management-studio -y
 choco upgrade sysinternals -y
+@echo choco upgrade teamviewer -y
 choco upgrade TortoiseGit -y
 @echo choco upgrade tortoisesvn -y
 choco upgrade VisualStudioCode -y
 choco upgrade vlc -y
+@echo choco upgrade windirstat -y
 choco upgrade winmerge -y
 choco upgrade winscp -y
 @echo choco upgrade vcredist2013 -y
 @echo choco upgrade vcredist2017 -y
+choco upgrade zip -y
+choco upgrade unzip -y
 @pause
